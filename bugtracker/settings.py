78eb4f131d7676f7922c6418274627ddf33b0d68
FariaFiza <23201145@uap-bd.edu>
2026-05-21 10:17:38 +0600

    first

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -14,8 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'bugs'
-
+    'bugs',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +54,10 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_URL = '/accounts/login/'
